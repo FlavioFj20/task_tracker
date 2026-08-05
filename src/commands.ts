4007301d1b497e7cc8ff91b812ea1e68c054f8a4
FlavioFj18 <fuxiflavio86@gmail.com>
2026-08-05 16:06:11 +0100

    feat(command): implement command handling with dedicated functions

---
+++ src/commands.ts
@@ -0,0 +1,132 @@
+import { parseArgs } from "node:util";
+
+function handleAddCommand(arg: string | undefined, extra: string[] | undefined) {
+    if (!arg) {
+      console.error("Error: The 'add' command a description.");
+      console.log('Example: test add "Buy groceries"');
+      process.exit(1);
+    }
+    
+    console.log(`✅ Adding item: "${arg}"`);
+    
+    if (extra) {
+      console.warn(`Warning: Extra arguments ignored: ${extra.join(", ")}`);
+    }
+}
+
+function handleUpdateCommand(id: number | undefined, args: string[] | undefined){ 
+
+    if (!args || args.length < 1) {
+        console.log('Warning: Not enough arguments');
+        process.exit(1);
+    }
+    console.log(`✅ Updating item: "${args[0]}"`);
+    if (args.length > 1) {
+      console.warn(`Warning: Extra arguments ignored: ${args.slice(1).join(", ")}`);
+    }
+}
+
+function handleDeleteCommand(id: number | undefined, args: string[] | undefined)
+{
+  if (!id) {
+    console.error("Error: Inform the ID or name of the item to delete.");
+    process.exit(1);
+  }
+  console.log(`🗑️  Deleting item: "${id}"`);
+  if (args && args.length > 0) {
+    console.warn(`Warning: Extra arguments ignored: ${args.slice(1).join(", ")}`);
+  }
+}
+
+function handleMarkCommand(id: number | undefined, args: string[] | undefined, status: boolean)
+{
+  if (!id) {
+    console.error("Error: Inform the ID or name of the item to delete.");
+    process.exit(1);
+  }
+  if (status)
+    console.log(`Marking item as done '${id}'.`);
+  else
+    console.log(`Marking in progress item '${id}'.`);
+  if (args && args.length > 0) {
+    console.warn(`Warning: Extra arguments ignored: ${args.slice(1).join(", ")}`);
+  }
+}
+
+function handleListCommand(arg: string | undefined){
+    switch(arg)
+    {
+        case "done":
+            console.log("📋 List all done items...");
+            break;
+        case "todo" :
+            console.log("📋 List all to do items...");
+            break;
+        case "in-progress" :
+            console.log("📋 List all in-progress items...");
+            break;
+        default:
+            if (!arg)
+                console.log("📋 List all of items...");
+            else {
+                console.error(`Error: unknown status'${arg}'.`);
+                console.log("Valid status: done, todo, in-progress");
+                process.exit(1);
+            }
+    }
+}
+
+function main() {
+  try {
+    const { positionals } = parseArgs({
+      options: {},
+      allowPositionals: true,
+      strict: true,
+    });
+ 
+    const [command, mainArg, ...otherArgs] = positionals;
+
+    if (!command) {
+      console.error("Error: Any command given.");
+      console.log("Usage correct: test <command> <argument>");
+      process.exit(1);
+    }
+
+    switch (command.toLowerCase()) {
+      case "add":
+        handleAddCommand(mainArg, otherArgs);
+        break;
+
+      case "update":
+        handleUpdateCommand(Number(mainArg), otherArgs);
+        break;
+
+      case "delete":
+        handleDeleteCommand(Number(mainArg), otherArgs);
+        break;
+
+      case "list":
+        handleListCommand(mainArg);
+        break;
+
+      case "mark-in-progress":
+        handleMarkCommand(Number(mainArg), otherArgs, false);
+        break;
+    
+      case "mark-done":
+        handleMarkCommand(Number(mainArg), otherArgs, true);
+        break;
+    
+      default:
+        console.error(`Error: unknown command'${command}'.`);
+        console.log("Valid commands: add, list, remove");
+        process.exit(1);
+    }
+
+  } catch (error: any) {
+    console.error(`Unexpected error: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+export default main;
